f3ce08c4f3f6b041ff2bfd1e68cad3286f3d7738
Dylan Lee <dylanfmlee@gmail.com>
2022-01-30 14:14:07 +0000

    further layout & tweet data

--- client/src/components/Tweets/Tweet/Tweet.js
+++ client/src/components/Tweets/Tweet/Tweet.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+
+import useStyles from './styles';
+import picture from '../../../images/twitterlogo.png';
 
 const Tweet = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Tweet</h1>
-        </div>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={picture} />
+            <div className={classes.overlay}>
+                <Typography variant='h6'>Username</Typography>
+                <Typography variant='body2'>PostDate</Typography>
+            </div>
+            <CardContent>
+                <Typography variant='body2' component="p" >Text here</Typography>
+            </CardContent>
+        </Card>
     );
 }
 
--- client/src/components/Tweets/Tweets.js
+++ client/src/components/Tweets/Tweets.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 
 import Tweet from './Tweet/Tweet';
+import useStyles from './styles';
 
 const Tweets = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <Tweet />
-        </div>
+        <Grid className={classes.mainContainer} container alignItems='stretch' spacing={3}>
+            <Grid item xs={11}>
+                <Tweet />
+            </Grid>
+        </Grid>
     );
 }
 
